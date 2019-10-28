039cbe2a1b834c57a95915cd8e29fb27681aff67
John <lzy2099@126.com>
2019-10-28 15:20:21 +0800

    add tab

--- print_lol.py
+++ print_lol.py
@@ -1,8 +1,10 @@
-def print_lol(movies):
+def print_lol(movies, level):
 
     for moive in movies:
 
         if isinstance(moive, list):
-           print_lol(moive)
+           print_lol(moive, level+1)
         else:
+            for tab_stop in range(level):
+                print("\t", end="")
             print(moive)
